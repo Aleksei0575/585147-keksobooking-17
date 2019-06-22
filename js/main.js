ef0b6c381319bae0b7610f7b906703305a8a487c
Aleksei0575 <chudnovets0575@gmail.com>
2019-06-22 12:17:47 +0300

    Устранил замечания наставника

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 'use strict';
 var BLOCK_WIDTH = document.querySelector('.map__pins').offsetWidth;
+// var PIN_MARKERS = document.querySelectorAll('.map__pin');
+
 
 // Генерация рандомного числа
 var generateNum = function (min, max) {
@@ -44,6 +46,7 @@ var mapBlock = document.querySelector('.map');
 var pinMarker = document.querySelector('#pin').content.querySelector('.map__pin');
 var renderPin = function (user) {
   var pinElement = pinMarker.cloneNode(true);
+  // pinElement.style = 'left:' + (32.5 + user.location.x) + 'px;top:' + (81 + user.location.y) + 'px;';
   pinElement.style = 'left:' + user.location.x + 'px;top:' + user.location.y + 'px;';
   pinElement.querySelector('img').setAttribute('src', user.author.avatar);
   pinElement.querySelector('img').setAttribute('alt', 'Здесь будет объявление');
@@ -65,35 +68,36 @@ pinUsers.appendChild(fragment);
 var formFillingInfo = document.querySelector('.ad-form--disabled');
 var searchForm = document.querySelector('.ad-form');
 var fieldsets = searchForm.getElementsByTagName('fieldset');
+var PIN_MARKERS = document.querySelectorAll('.map__pin');
 
 // Функция добавляет атрибут disabled полям формы и прячет маркеры пользователей
-var blockElemet = function (element) {
+var blockElement = function (element) {
   for (var j = 0; j < element.length; j++) {
     element[j].setAttribute('disabled', 'disabled');
   }
-  // pinUsers = document.querySelectorAll('.map__pin').style = 'display: none;';
-  pinUsers = document.querySelectorAll('.map__pin');
-  for (var k = 1; k < pinUsers.length; k++) {
-    var allMarker = pinUsers[k];
+  // pinUsers = document.querySelectorAll('.map__pin');
+  for (var k = 1; k < PIN_MARKERS.length; k++) {
+    var allMarker = PIN_MARKERS[k];
     allMarker.classList.add('visually-hidden');
   }
   return element;
 };
-blockElemet(fieldsets);
+blockElement(fieldsets);
 
 // Перевод страницы в активный режим
 // Функция убирает атрибут disabled у полей формы и добавляет маркеры пользователей
-var pinPageActivate = document.querySelector('.map__pin--main');
-pinPageActivate.addEventListener('click', function () {
+var onPinPageActivateClick = document.querySelector('.map__pin--main');
+onPinPageActivateClick.addEventListener('click', function (evt) {
+  evt.preventDefault();
   mapBlock.classList.remove('map--faded');
   formFillingInfo.classList.remove('ad-form--disabled');
   var blockActivate = function (element) {
     for (var j = 0; j < element.length; j++) {
-      element[j].removeAttribute('disabled', 'disabled');
+      element[j].removeAttribute('disabled');
     }
-    pinUsers = document.querySelectorAll('.map__pin');
-    for (var k = 1; k < pinUsers.length; k++) {
-      var allMarker = pinUsers[k];
+    // pinUsers = document.querySelectorAll('.map__pin');
+    for (var k = 1; k < PIN_MARKERS.length; k++) {
+      var allMarker = PIN_MARKERS[k];
       allMarker.classList.remove('visually-hidden');
     }
     return element;
@@ -102,7 +106,7 @@ pinPageActivate.addEventListener('click', function () {
 });
 
 // Функция получения координат
-function getСoordinates(elem) {
+function getCoordinates(elem) {
   var boxObject = document.querySelector(elem).getBoundingClientRect();
   return {
     top: boxObject.top + pageYOffset,
@@ -110,12 +114,12 @@ function getСoordinates(elem) {
   };
 }
 
-var mapPinCoordinates = getСoordinates('.map__pin');
+var mapPinCoordinates = getCoordinates('.map__pin');
 
 // Функция получения меткой координаты и ее передача input в форме
-var setСoordinates = function () {
+var setCoordinates = function () {
   var coordX = mapPinCoordinates.left;
   var coordY = mapPinCoordinates.top;
   document.querySelector('#address').value = coordX + ',' + coordY;
 };
-setСoordinates('#address');
+setCoordinates('#address');
