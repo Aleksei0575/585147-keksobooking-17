df52f92a57030c95c98825785a50ec4450a4cf8f
Aleksei0575 <chudnovets0575@gmail.com>
2019-07-01 18:17:28 +0300

    Сделал модуль util.js

---
+++ js/util.js
@@ -0,0 +1,27 @@
+'use strict';
+
+// В этом файле находятся вспомогательные функции
+
+(function () {
+  window.util = {
+    MAP_BLOCK: document.querySelector('.map'), // Поиск карты с объявлениями
+    searchForm: document.querySelector('.ad-form'), // Поиск формы добавления объявлений
+    // Функция будет генерировать случайный элемент из массива
+    getGenerateArrayItem: function (arr) {
+      return arr[Math.floor(Math.random() * arr.length)];
+    },
+    // Функция будет генерировать случайный число
+    getGenerateNum: function (min, max) {
+      return Math.floor(min - 0.5 + Math.random() * (max - min + 1));
+    },
+    // Функция добавления элементам DOM атрибута disabled
+    addDisabled: function (el) {
+      el.disabled = true;
+    },
+    // Функция убирает атрибут disabled у DOM-элементов
+    removeDisabled: function (el) {
+      el.disabled = false;
+    }
+  };
+
+})();
