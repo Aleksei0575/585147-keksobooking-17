4cfd9c8d985989da6f2563a82ad1fa5309a47f60
Aleksei0575 <chudnovets0575@gmail.com>
2019-07-02 16:01:00 +0300

    Сделал модуль data.js

---
+++ js/data.js
@@ -0,0 +1,36 @@
+'use strict';
+
+// В этом модуле создаются данные для объявлений случайных пользователей
+(function () {
+  // Параметры координаты метки других пользователей
+  var LocationMarkerX = {
+    MIN: window.param.PIN_WIDTH / 2,
+    MAX: window.util.MAP_BLOCK.offsetWidth - window.param.PIN_WIDTH / 2
+  };
+
+  // Функция создает одного пользователя
+  var createUser = function (adsNumber) {
+    return {
+      'autor': {
+        'avatar': 'img/avatars/user' + adsNumber + '.png'
+      },
+
+      'offer': {
+        'type': window.util.getGenerateArrayItem(window.param.OFFER)
+      },
+
+      'location': {
+        'x': window.util.getGenerateNum(LocationMarkerX.MIN, LocationMarkerX.MAX),
+        'y': window.util.getGenerateNum(window.param.LocationY.MIN, window.param.LocationY.MAX)
+      }
+    };
+  };
+
+  // Создание массива из случайных пользователей
+  var randomAdsUsers = [];
+  for (var i = 0; i < window.param.NUMBER_ADS; i++) {
+    randomAdsUsers.push(createUser('0' + (i + 1)));
+  }
+  window.data = randomAdsUsers;
+
+})();
