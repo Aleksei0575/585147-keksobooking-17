2b4bddc7c36d9c7d7edc1d0148d0ae07faaec310
Aleksei0575 <chudnovets0575@gmail.com>
2019-07-05 20:48:11 +0300

    Подкорректировал модули active-page.js, param.js, pin.js чтобы отображались данные с сервера

--- js/pin.js
+++ js/pin.js
@@ -11,18 +11,23 @@
     var pinElement = pinMarkerTemplate.cloneNode(true);
     pinElement.style.left = user.location.x - window.param.PIN_WIDTH / 2 + 'px';
     pinElement.style.top = user.location.y - window.param.PIN_HEIGHT + window.param.MAIN_PIN_SIZE + 'px';
-    pinElement.querySelector('img').setAttribute('src', user.author.avatar);
-    pinElement.querySelector('img').setAttribute('alt', 'Здесь будет объявление');
+
+    var pinElementImg = pinElement.querySelector('img');
+    pinElementImg.src = user.author.avatar;
+    pinElementImg.alt = 'Здесь будет объявление';
     return pinElement;
   };
 
   // Вставка маркеров на карту
-  var users = window.data.createUsers(window.param.NUMBER_ADS);
+  // var users = window.data.createUsers(window.param.NUMBER_ADS);
   window.pin = {
-    getUsers: function (quantityAds) {
+    getUsers: function (arrAds) {
       var fragment = document.createDocumentFragment();
-      for (var i = 0; i < quantityAds; i++) {
-        fragment.appendChild(renderPin(users[i]));
+      for (var i = 0; i < arrAds.length; i++) {
+        if (arrAds[i].offer) {
+          fragment.appendChild(renderPin(arrAds[i]));
+        }
+
         pinUsers.appendChild(fragment);
       }
     }
