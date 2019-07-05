7fa596337aa6ae7bebc829b134c7d0e5662ad23f
Aleksei0575 <chudnovets0575@gmail.com>
2019-07-05 23:49:46 +0300

    Добавил в модуль с сообщениями - сообщение об успешной отправке

--- js/messages.js
+++ js/messages.js
@@ -6,8 +6,42 @@
   var ESC_KEYCODE = 27;
 
   var errorTemplate = document.querySelector('#error').content.querySelector('.error');
+  var successTemplate = document.querySelector('#success').content.querySelector('.success');
 
   window.messages = {
+    // Функция вывода сообщения об успешной отправке объявления
+    onSuccess: function (message) {
+      var successPatern = successTemplate.cloneNode(true);
+      var successMessage = successPatern.querySelector('.success__message');
+      successMessage.textContent = message;
+
+      // Добавляем сообщение в разметку
+      mainTeg.insertAdjacentElement('afterbegin', successPatern);
+
+      // Функция удаляет сообщение об успешном создании объявления
+      var delSuccess = function (evt) {
+        evt.preventDefault();
+        successPatern.remove();
+      };
+
+      // Функция закрывает окно с успешной отправкой
+      // по нажатию на клавишу esc
+      var keydownEsc = function (evt) {
+        if (evt.keyCode === ESC_KEYCODE) {
+          delSuccess(evt);
+          document.removeEventListener('keydown', keydownEsc);
+        }
+      };
+      // по нажатию на произвольную область окна
+      var onAreaClick = function (evt) {
+        delSuccess(evt);
+        document.removeEventListener('click', onAreaClick);
+      };
+
+      document.addEventListener('keydown', keydownEsc);
+      document.addEventListener('click', onAreaClick);
+    },
+
     // Функция вывода сообщения об ошибке
     onError: function (message) {
       var errorPatern = errorTemplate.cloneNode(true); // клонирование шаблона из разметки
@@ -37,8 +71,15 @@
           document.removeEventListener('keydown', keydownEsc);
         }
       };
+      // по нажатию на произвольную область окна
+      var onAreaClick = function (evt) {
+        delError(evt);
+        document.removeEventListener('click', onAreaClick);
+      };
+
       errorButton.addEventListener('click', buttonClick);
       document.addEventListener('keydown', keydownEsc);
+      document.addEventListener('click', onAreaClick);
     }
   };
 })();
