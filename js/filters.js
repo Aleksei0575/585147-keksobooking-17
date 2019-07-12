ea777cb7b88d2301063d1c987921ea49e6256e72
Aleksei0575 <chudnovets0575@gmail.com>
2019-07-12 15:58:14 +0300

    Устранил замечания в модуле фильтрации, добавил фильтрацию по всем параметрам

--- js/filters.js
+++ js/filters.js
@@ -7,92 +7,121 @@
     mapFilters: window.util.MAP_BLOCK.querySelector('.map__filters')
   };
 
-  var housingType = window.filters.mapFilters.querySelector('#housing-type');
-  var housingPrice = window.filters.mapFilters.querySelector('#housing-price');
-
-  // Подготовлено, но пока еще не реализовано => будет реализовано в следующем задании
-  // var housingRooms = window.filters.mapFilters.querySelector('#housing-rooms');
-  // var housingGuests = window.filters.mapFilters.querySelector('#housing-guests');
-
-  // присваиваем текущее значение переменным
-  var typeValue = housingType.value;
-  var priceValue = housingPrice.value;
-
-  // Подготовлено, но пока еще не реализовано => будет реализовано в следующем задании
-  // var roomsValue = housingRooms.value;
-  // var guestsValue = housingGuests.value;
-
-  // невыбранное значение фильтра
-  var anyValue = 'any';
-
-  // массив в котором будут храниться отфильтрованные зачения
-  var filterDataValue = [];
-
-  // Функция отрисовки отфильтрованных объявлений
-  // var updateDatesList = function () {
-  //   window.pin.getUsers(filterDataValue.slice(0, (window.param.MAX_ADS_QUANTITY)));
-  // };
-
-  // объект для хранения зависимости выбранного фильтра и функции, изменяющей соответствующую переменную
-  // var selectedValueAndFunc = {
-  //   'housing-type': function (val) {
-  //     typeValue = val;
-  //   },
-  //   'housing-price': function (val) {
-  //     priceValue = val;
-  //   }
-  // };
-
-  // объект переводит значаение "Стоимость" в цифры
-  var priceNumberMap = {
-    'low': {
-      min: 0,
-      max: 1000
-    },
-
-    'middle': {
-      min: 10000,
-      max: 50000
-    },
-
-    'high': 50000
-  };
-
-  // Функция сравнения отдельных параметров фильтрации
-  // Функция проверяет выбранное в фильтре значение
-  var checkTypeChange = function (value, type) {
-    // если фильтр еще не применен или значение соответствует выбранному типу
-    if (value === anyValue || type === value) {
-      return true;
-    }
-    // убирает метки не соответствующие выбранному типу жилья
-    return !!window.pin.clear();
-  };
-
-  // сравнивает стоимость жилья
-  var checkPriceChange = function (it) {
-    if (priceValue === anyValue) {
-      return true;
-    } else if (priceValue === 'high') {
-      return it.offer.price >= priceNumberMap[priceValue];
-    }
-    return it.offer.price >= priceNumberMap[priceValue].min && it.offer.price < priceNumberMap[priceValue].max;
-  };
-
   // Добавим обработчик события на всю форму
   window.filters.mapFilters.addEventListener('change', function () {
-    // selectedValueAndFunc[evt.target.name](evt.target.value);
-    typeValue = housingType.value;
-    priceValue = housingPrice.value;
+    // Списки параметров фильтрации в разметке
+    var housingType = window.filters.mapFilters.querySelector('#housing-type');
+    var housingPrice = window.filters.mapFilters.querySelector('#housing-price');
+    var housingRooms = window.filters.mapFilters.querySelector('#housing-rooms');
+    var housingGuests = window.filters.mapFilters.querySelector('#housing-guests');
 
-    // Подготовлено но пока еще не реализовано => будет реализовано в следующем задании
-    // roomsValue = housingRooms.value;
-    // guestsValue = housingGuests.value;
+    // блок с удобствами в разметке
+    var wifi = window.filters.mapFilters.querySelector('#filter-wifi');
+    var dishwasher = window.filters.mapFilters.querySelector('#filter-dishwasher');
+    var parking = window.filters.mapFilters.querySelector('#filter-parking');
+    var washer = window.filters.mapFilters.querySelector('#filter-washer');
+    var elevator = window.filters.mapFilters.querySelector('#filter-elevator');
+    var conditioner = window.filters.mapFilters.querySelector('#filter-conditioner');
+
+    // присваиваем значение переменным по умолчанию
+    var typeValue = housingType.value;
+    var priceValue = housingPrice.value;
+    var roomsValue = housingRooms.value;
+    var guestsValue = housingGuests.value;
+
+    // невыбранное значение фильтра
+    var anyValue = 'any';
+
+    // массив в котором будут храниться отфильтрованные зачения
+    var filterDataValue = [];
+
+    // объект переводит значаение "Стоимость" в цифры
+    var priceNumberMap = {
+      'low': {
+        min: 0,
+        max: 1000
+      },
+
+      'middle': {
+        min: 10000,
+        max: 50000
+      },
+
+      'high': 50000
+    };
+
+    // Функция сравнения отдельных параметров фильтрации
+    // Функция проверяет выбранное в фильтре значение
+    var checkTypeChange = function (value, type) {
+      // если фильтр еще не применен или значение соответствует выбранному типу
+      if (value === anyValue || type === value) {
+        return true;
+      }
+      // убирает метки не соответствующие выбранному типу жилья
+      return !!window.pin.clear();
+    };
+
+    // сравнивает стоимость жилья
+    // var checkPriceChange = function (it) {
+    //   if (priceValue === anyValue) {
+    //     return true;
+    //   } else if (priceValue === 'high') {
+    //     return it.offer.price >= priceNumberMap[priceValue];
+    //   }
+    //   return it.offer.price >= priceNumberMap[priceValue].min && it.offer.price < priceNumberMap[priceValue].max;
+    // };
+
+    // сравнивает стоимость жилья
+    var checkPriceChange = function (value, price) {
+      if (value === anyValue) {
+        return true;
+      } else if (value === 'high') {
+        return price >= priceNumberMap[priceValue];
+      }
+      return price >= priceNumberMap[priceValue].min && price < priceNumberMap[priceValue].max;
+    };
+
+    // сравнивает количество комнат
+    var checkRoomsChange = function (value, rooms) {
+      // если фильтр еще не применен или значение соответствует выбранному количеству комнат
+      if (value === anyValue || rooms.toString() === roomsValue) {
+        return true;
+      }
+      // убирает метки не соответствующие выбранному количеству комнат
+      return !!window.pin.clear();
+    };
+
+    // сравнивает число гостей
+    var checkGuestsChange = function (value, guests) {
+      // если фильтр еще не применен или значение соответствует выбранному количеству комнат
+      if (value === anyValue || guests.toString() === guestsValue) {
+        return true;
+      }
+      // убирает метки не соответствующие выбранному количеству комнат
+      return !!window.pin.clear();
+    };
+
+    // функция сравнивает выбранные удобства
+    var checkFacilities = function (input, el) {
+      if (!input.checked || el.offer.features.indexOf(input.value) !== -1) {
+        return true;
+      }
+      return !!window.pin.clear();
+    };
 
     // фильтрация данных
     filterDataValue = window.param.datesList.filter(function (ad) {
       // return checkTypeChange(ad) && checkPriceChange(ad);
-      return checkTypeChange(typeValue, ad.offer.type) && checkPriceChange(ad);
+      return checkTypeChange(typeValue, ad.offer.type) &&
+        checkPriceChange(priceValue, ad.offer.price) &&
+        checkRoomsChange(roomsValue, ad.offer.rooms) &&
+        checkGuestsChange(guestsValue, ad.offer.guests) &&
+        checkFacilities(wifi, ad) &&
+        checkFacilities(dishwasher, ad) &&
+        checkFacilities(parking, ad) &&
+        checkFacilities(washer, ad) &&
+        checkFacilities(elevator, ad) &&
+        checkFacilities(conditioner, ad);
     });
 
     // отрисовка меток после фильтрации
