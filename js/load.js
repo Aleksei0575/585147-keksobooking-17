0e0dee19dc4c800a9188b2b0a8c497e590079a1b
Aleksei0575 <chudnovets0575@gmail.com>
2019-07-20 09:03:27 +0300

    Внес изменения в модуль, при успешной отправке показывать сообщение, что данные успешно загружены

--- js/load.js
+++ js/load.js
@@ -2,9 +2,10 @@
 
 // Модуль загрузки файлов с сервера
 (function () {
-  var URL = 'https://js.dump.academy/keksobooking/data'; // Адрес сервера
+  var URL_DATA_LOADING = 'https://js.dump.academy/keksobooking/data'; // Адрес загрузки данных с сервера
+  var URL_DATA_SAVING = 'https://js.dump.academy/keksobooking'; // Адрес отправки данных формы на сервер
 
-  // Функуия загрузки данных с сервера
+  // Функция загрузки данных с сервера
   window.load = function (onSuccess, onError) {
     var xhr = new XMLHttpRequest();
     xhr.responseType = 'json';
@@ -22,7 +23,29 @@
       onError('Запрос не успел выполнится за ' + xhr.timeout + 'мс');
     });
     xhr.timeout = 10000; // выставлен таймаут в 10 с
-    xhr.open('GET', URL); // Указываем как и куда обращаемся
+    xhr.open('GET', URL_DATA_LOADING); // Указываем как и куда обращаемся
+    xhr.send(); // Запускаем процесс отправки запроса на сервер
+  };
+
+  // Функция отправки данных на сервер
+  window.save = function (onSuccess, onError) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+    // Обработка события загрузки
+    xhr.addEventListener('load', function () {
+      if (xhr.status === 200) {
+        return onSuccess(xhr.response);
+      }
+      return onError('Ошибка загрузки объявления. Статус ответа: ' + xhr.status + '' + xhr.statusText);
+    });
+    xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения');
+    });
+    xhr.addEventListener('timeout', function () {
+      onError('Запрос не успел выполнится за ' + xhr.timeout + 'мс');
+    });
+    xhr.timeout = 50000; // выставлен таймаут в 10 с
+    xhr.open('POST', URL_DATA_SAVING); // Указываем как и куда обращаемся
     xhr.send(); // Запускаем процесс отправки запроса на сервер
   };
 })();
